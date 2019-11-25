3dec9ff497ad525e7b6cf37d0beda3988f3952f0
enviz <48526315+enviz@users.noreply.github.com>
2019-11-25 18:43:04 +0530

    Update usage_api.py

--- usage_api.py
+++ usage_api.py
@@ -1,5 +1,5 @@
 import requests
-image_path = 'receipt.jpeg'
+image_path = 'your_image.jpeg'
 data = {'file': open(image_path, 'rb')}
 response = requests.post('https://extract-date-receipt.herokuapp.com/extract_date', files=data)
 print(response.json())
